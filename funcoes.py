d056bb517e098909dd4e4076c80c83f26ac2e239
miguel <migas.eust@gmail.com>
2020-10-19 20:56:15 -0300

    biblioteca de funções funcional

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,16 @@
 #Define biblioteca da GPIO
 import RPi.GPIO as GPIO
- 
+
 #Define biblioteca de tempo
 import time                           
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-lista_botoes = [16]
+lista_botoes = [16, 18]
+canais = {1:18}
  
 
 def inicializa_controle():
-	
 	for pino in lista_botoes:
 		GPIO.setup(pino, GPIO.OUT)
 		GPIO.output(pino, 1)
@@ -18,14 +18,21 @@ def inicializa_controle():
 	GPIO.output(12, 1)
 	
 def finaliza_controle():
-	
 	GPIO.output(12, 0)
 	for pino in lista_botoes:
 		GPIO.output(pino, 0)
 	 
-def aperta_botao(pino):
+def aperta_botao(pino, tempo):
 	GPIO.output(pino, 0)
-	time.sleep(0.5)
+	time.sleep(tempo)
 	GPIO.output(pino, 1)
 
+def comando(canal, botao):
+	aperta_botao(canais[canal], 0.3)
+	time.sleep(0.5)
+	aperta_botao(botao, 0.3)
 
+def aprende_comando(canal, botao):
+	aperta_botao(canais[canal], 2)
+	time.sleep(1)
+	aperta_botao(botao, 0.3)
