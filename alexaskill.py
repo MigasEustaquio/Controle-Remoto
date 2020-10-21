483d746ae5133bf338262077d19c504a2a46a8fa
miguel <migas.eust@gmail.com>
2020-10-20 21:41:07 -0300

    incremento de funções no código principal

--- alexaskill.py
+++ alexaskill.py
@@ -4,7 +4,10 @@ from funcoes.py import *
  
 app = Flask(__name__)
 ask = Ask(app, '/')
- 
+
+statusTela = 'off'
+statusTV = 'off'
+
 @ask.intent('LedIntent')
 def led(color, status):
   if color.lower() not in pins.keys():
@@ -32,6 +35,38 @@ def desligarLuz():
     return statement('Turning tv off')
 
 
+@ask.intent('LigarTela')
+def ligarTela(status):
+    if status == 'on':
+      if statusTela == 'off':
+        comando(0, 0) #mandar canal e botao respectivamente no parametro
+        statusTela = 'on'
+        return statement('Turning screen on')
+      else:
+        return statement('Screen is already turned on')
+
+    if status == 'off':
+      if statusTela == 'on':
+        comando(0, 0) #mandar canal e botao respectivamente no parametro
+        statusTela = 'off'
+        return statement('Turning screen off')
+      else:
+        return statement('Screen is already turned off')
+
+
+@ask.intent('MudarVolume')
+def mudarVolume(volume):
+    if volume == 'up':
+      comando(0,0) #mandar canal e botao respectivamente no parametro
+      return statement('Turning the volume {}'.format(volume))
+    else:
+      comando(0,0) #mandar canal e botao respectivamente no parametro
+      return statement('Turning the volume {}'.format(volume))
+
+    
+
+
+
 @ask.intent('LightIntent')
 def desligarLuz(status):
     GPIO.setmode(GPIO.BOARD)
@@ -43,10 +78,6 @@ def desligarLuz(status):
 if __name__ == '__main__':
   app.run(debug=True,port=6200)
   try:
-    GPIO.setmode(GPIO.BCM)
-    pins = {'red':12, 'yellow':10, 'green':11}
-    for pin in pins.values(): 
-      GPIO.setup(pin, GPIO.OUT)
     app.run(debug=True)
   finally:
     GPIO.cleanup()
