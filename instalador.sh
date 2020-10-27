ffade068437dbc9af47e46ad781c3bfd49f871b2
MigasEustaquio <31831761+MigasEustaquio@users.noreply.github.com>
2020-10-27 13:43:23 -0300

    added color

--- instalador.sh
+++ instalador.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-printf "\nAtualizando repositórios...\n\n"
+RED='\033[0;31m'
+GRE='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "\n${NC}Atualizando repositórios...\n\n"
 if ! apt-get update
 then
-    printf "\nNão foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list\n\n"
+    printf "\n${RED}Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list\n\n"
     exit 1
 fi
-printf "\nAtualização feita com sucesso\n\n"
+printf "\n${GRE}Atualização feita com sucesso\n\n"
 
 
-printf "\nAtualizando pacotes já instalados\n\n"
+printf "\n${NC}Atualizando pacotes já instalados\n\n"
 if ! apt-get upgrade
 then
-    printf "\nNão foi possível atualizar pacotes.\n\n"
+    printf "\n${RED}Não foi possível atualizar pacotes.\n\n"
     exit 1
 fi
-printf "\nAtualização de pacotes feita com sucesso.\n\n"
+printf "\n${GRE}Atualização de pacotes feita com sucesso.\n\n"
 
 
 
